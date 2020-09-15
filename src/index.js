40addcbd8515ec4fe9659b2c671d4ed868d03bd7
patidarashu6 <patidarashu6@gmail.com>
2020-09-15 21:04:38 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,141 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+//import Header from './Header.js';
+//import MidSaction from './MidSaction.js';
+//import Footer from './Footer.js';
+
+
+
+class Header extends Component {
+  render() {
+      return <>
+
+          <header className="text-gray-700 body-font">
+              <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                  <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+                          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                      </svg>
+                      <span className="ml-3 text-xl">PatidarInfoTech</span>
+                  </a>
+                  <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                      <a href="/index.html" className="mr-5 hover:text-gray-900">Home</a>
+                      <a className="mr-5 hover:text-gray-900">Services</a>
+                      <a className="mr-5 hover:text-gray-900">About</a>
+                      <a href="/tamplates/contactus.html" className="mr-5 hover:text-gray-900">Contact</a>
+                  </nav>
+                  <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Login
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                          <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                  </button>
+              </div>
+          </header>
+      </>
+  }
+}
+export default Header;
+
+export class MidSaction extends Component {
+  render() {
+      return <>
+          <section class="text-gray-700 body-font">
+              <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                  <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                      <img src="https://source.unsplash.com/720x600/?coding,computer,technology" />
+                  </div>
+                  <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                          Before they sold out
+            <br class="hidden lg:inline-block" />readymade gluten
+              </h1>
+                      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                      <div class="flex justify-center">
+                          <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+                          <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Button</button>
+                      </div>
+                  </div>
+              </div>
+          </section>
+      </>
+  }
+}
+// export default MidSaction;
+
+export class Footer extends Component{
+    render(){
+      let currDate = new Date(2020, 5, 5, 1 );
+      currDate = currDate.getHours();
+      let msg = '';
+      const msgStyle = { fontWeight: "bold"};
+
+      if(currDate >= 5 &&  currDate < 12){
+        msg = 'Good Morning';
+        msgStyle.color = "green";
+        
+      }
+      else if(currDate >= 12 && currDate < 16){
+        msg = 'Good Afternoon';
+        msgStyle.color = "yellow";
+      }
+      else if(currDate >= 16 && currDate < 18){
+        msg = 'Good Evening';
+        msgStyle.color = "orange";
+      }
+      else{
+        msg = 'Good Night';
+        msgStyle.color = "blue";
+      }
+      return <>
+      <footer class="text-gray-700 body-font">
+      <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-no-wrap flex-wrap flex-col">
+        <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+    <p>{currDate}</p><p >Hello <span style={msgStyle}>{msg} </span>Ashu !</p>
+        </div>
+      </div>
+      <div class="bg-gray-200">
+        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 tailblocks —
+            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+          </p>
+          <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a class="text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
+    </footer>
+      </>
+  }
+}
+// export default Footer;
+
+ReactDOM.render(
+  <>
+    <Header />
+    <MidSaction />
+    <Footer />
+  </>
+,
+  document.getElementById('root')
+);
